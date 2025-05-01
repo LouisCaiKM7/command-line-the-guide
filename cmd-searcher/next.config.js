9f3e2b861c4a2a0f5f1cb88b0df97edc55876057
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-05-01 13:44:16 +0800

    Revert "Update next.config.js"
    This reverts commit 4938d5b945c547be863f9b71186e655c0f09bd04.

--- cmd-searcher/next.config.js
+++ cmd-searcher/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   // Make sure to update this to your GitHub repository name
-  basePath: process.env.NODE_ENV === 'production' ? '/command-line-the-guide' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/command-line-the-guide/' : '',
+  basePath: '/command-line-the-guide',
+  assetPrefix: '/command-line-the-guide/',
 }
 
 module.exports = nextConfig
